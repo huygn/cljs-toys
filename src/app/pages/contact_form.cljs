a73e9a5bd66cd7ec756eb021dc7be1bcf9e47d58
Huy Giang <gnhuy91@gmail.com>
2018-09-29 22:40:48 +0700

    rewrite using mixin

--- src/app/pages/contact_form.cljs
+++ src/app/pages/contact_form.cljs
@@ -40,80 +40,85 @@
                                  :value true
                                  :visited true})
 
-(defn make-field [form]
-  (rum/defcs field <
-    rum/reactive
-    {:init (fn [state]
-             (let [field-name (-> state :rum/args first :name)
-                   subscription (-> state :rum/args first :subscription (or field-subscription-default))
-                   field-state (atom {})
-                   unsubscribe (.registerField form
-                                               field-name
-                                               #(reset! field-state %)
-                                               (clj->js subscription))]
-               (assoc state
-                      :unsubscribe unsubscribe
-                      :field-state field-state)))
-     :will-unmount (fn [state] ((:unsubscribe state)) state)}
-
-    [state {:keys [name]} render]
-    (let [field (rum/react (:field-state state))]
-      (js/console.log "field-render" field)
-      (render {:field-state field
-               :input {:name name
-                       :value (-> (.-value field) (or ""))
-                       :on-blur #(.blur field)
-                       :on-focus #(.focus field)
-                       :on-change #(.change field (-> % .-target .-value))}}))))
-
-(rum/defcs form <
-  rum/reactive
+(defn with-form [form-opt fields]
   {:init (fn [state]
-           (let [on-submit (-> state :rum/args first :on-submit)
-                 form (fform/createForm
-                       #js {:onSubmit on-submit})]
-             (assoc state :form form)))
+           (let [on-submit (:on-submit form-opt)
+                 initial-values (-> form-opt :initial-values clj->js)
+                 validate (:validate form-opt)
+                 form (fform/createForm #js {:onSubmit on-submit
+                                             :initialValues initial-values
+                                             :validate validate})]
+             (assoc state :form/form form)))
    :will-mount (fn [state]
-                 (let [form (:form state)
+                 (let [form (:form/form state)
                        form-state (atom {})
-                       subscription (-> state :rum/args first :subscription (or form-subscription-default))
-                       unsubscribe (.subscribe form
-                                               #(reset! form-state
-                                                        (js->clj % :keywordize-keys true))
-                                               (clj->js subscription))
-                       render-field (make-field form)]
+                       field-state (atom {})
+                       form-subscription (-> form-opt
+                                             :subscription
+                                             (or form-subscription-default))
+                       unsubscribe-form (.subscribe form
+                                                    #(reset!
+                                                      form-state
+                                                      (js->clj % :keywordize-keys true))
+                                                    (clj->js form-subscription))
+                       unsubscribe-fields (doall
+                                           (map (fn [{name :name, sub :subscription}]
+                                                  (.registerField
+                                                   form
+                                                   name
+                                                   #(swap! field-state assoc name
+                                                           (js->clj % :keywordize-keys true))
+                                                   (-> sub
+                                                       (or field-subscription-default)
+                                                       clj->js)))
+                                                fields))]
                    (assoc state
-                          :form-state form-state
-                          :render-field render-field)))
-   :will-unmount (fn [state] ((:unsubscribe state)) state)}
+                          :form/state form-state
+                          :fields/state field-state
+                          :form/unsubscribe unsubscribe-form
+                          :fields/unsubscribe unsubscribe-fields
+                          :form/submit! (fn [e]
+                                          (.preventDefault e)
+                                          (.submit form))
+                          :form/field (fn [name]
+                                        (let [field (get @field-state name)
+                                              {:keys [value change focus blur]} field
+                                              {:keys [active
+                                                      data
+                                                      dirty
+                                                      dirtySinceLastSubmit
+                                                      error
+                                                      initial
+                                                      invalid
+                                                      pristine
+                                                      submitError
+                                                      submitFailed
+                                                      submitSucceeded
+                                                      touched
+                                                      valid
+                                                      visited] :as meta} field]
+                                          {:input {:name name
+                                                   :value (or value "")
+                                                   :on-change #(-> % .-target .-value change)
+                                                   :on-focus #(focus)
+                                                   :on-blur #(blur)}
+                                           :meta meta})))))
+   :will-unmount (fn [state]
+                   ((:form/unsubscribe state))
+                   (doseq [unsub (:fields/unsubscribe state)] (unsub))
+                   state)})
 
-  [state opt render]
-  (let [form (:form state)
-        form-state (rum/react (:form-state state))
-        submit! (fn [e]
-                  (.preventDefault e)
-                  (.submit form))
-        render-field (:render-field state)]
-    (js/console.log "form render" form-state)
-    (render {:form form
-             :form-state form-state
-             :submit! submit!
-             :field render-field})))
-
-(rum/defc component []
-  (form {:on-submit (fn [v f]
-                      (js/console.log v f)
-                      (js/Promise. (fn [resolve]
-                                     (js/setTimeout
-                                      #(do (js/console.log "submitted")
-                                           (resolve))
-                                      500))))}
-        (fn [{:keys [form-state submit! field]}]
-          [:form {:on-submit submit!}
-           (field {:name "firstName"}
-                  (fn [{:keys [input]}]
-                    [:input (merge input {:class "border"})]))
-           [:button {:type "submit"
-                     :disabled (:submitting form-state)
-                     :class "px-3 py-2 text-white bg-blue rounded-sm"}
-            "Submit"]])))
+(rum/defcs component <
+  rum/reactive
+  (with-form
+    {:on-submit #(js/console.log %)}
+    [{:name "firstName"}])
+  [state]
+  (let [form (:form/form state)
+        form-state (rum/react (:form/state state))
+        submit! (:form/submit! state)
+        field (:form/field state)]
+    (js/console.log (field "firstName"))
+    [:form {:on-submit submit!}
+     [:input (merge {:class "border"}
+                    (:input (field "firstName")))]]))
